6202345e66274bb25d9ba4020fe5266d56707ed3
Stephen Smith <StephenASmith@protonmail.com>
2021-03-15 14:26:23 -0400

    clean up code with dedicated print function

--- pf2bot.py
+++ pf2bot.py
@@ -4,6 +4,22 @@ import queries
 
 URL = "https://us-central1-pf2-graphql.cloudfunctions.net/api/graphql"
 
+def pretty_print(data):
+    ret = ""
+    for e in data.keys():
+        if data[e] is None:
+            continue
+        if isinstance(data[e], list):
+            if isinstance(data[e][0], dict):
+              li = []
+              for x in data[e]:
+                li.append(x["name"])
+              data[e] = li
+            ret += "**" + e.title() + "**:" + "\n\t" + "\n\t".join(data[e]) + "\n"
+            continue
+        ret += "**" + e.title() + "**:" + "\n\t" + str(data[e]) + "\n"
+    return ret
+
 def get_spells():
     request = requests.post(URL, json={'query': queries.Spells_query})
     if request.status_code == 200:
@@ -18,18 +34,7 @@ def get_spell(s: str):
         if s.lower() in e["name"].lower():
             spell = e
             break
-    ret = ""
-    for e in spell.keys():
-        if spell[e] is None:
-            continue
-        if e in "traits":
-            ret += "**Traits:**\n\t"
-            for f in spell[e]:
-                ret += "\t" + f["name"]
-            ret += "\n"
-            continue
-        ret += "**" + e.title() + "**:" + "\n\t" + str(spell[e]) + "\n"
-    return ret
+    return pretty_print(spell)
 
 def get_conditions():
     request = requests.post(URL, json={'query': queries.cond_query})
@@ -52,15 +57,7 @@ def get_condition(s: str):
         if s.lower() in e["name"].lower():
             cond = e
             break
-    ret = ""
-    for e in cond.keys():
-        if cond[e] is None:
-            continue
-        if isinstance(cond[e], list):
-            ret += "**" + e.title() + "**:" + "\n\t" + "\n\t".join(cond[e]) + "\n"
-            continue
-        ret += "**" + e.title() + "**:" + "\n\t" + str(cond[e]) + "\n"
-    return ret
+    return pretty_print(cond)
 
 
 def get_trait(s: str):
@@ -70,12 +67,7 @@ def get_trait(s: str):
         if s.lower() in e["name"].lower():
             trait = e
             break
-    ret = ""
-    for e in trait.keys():
-        if trait[e] is None:
-            continue
-        ret += "**" + e.title() + "**:" + "\n\t" + str(trait[e]) + "\n"
-    return ret
+    return pretty_print(trait)
 
 
 
